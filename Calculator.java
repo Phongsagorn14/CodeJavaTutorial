cf3d2d29eed5f1da79316f2d929b658ce2d2aeba
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-07-31 14:56:40 +0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,37 +2,37 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("เลือกการคำนวณ : ");
-        System.out.println("1. บวกเลข");
-        System.out.println("2. คูณเลข");
-        System.out.println("3. หารเลข");
-        System.out.println("4. ลบเลข");
-        System.out.print("กรุณาเลือก 1-4 : ");
+        System.out.println("Select calculation : ");
+        System.out.println("1. Add numbers");
+        System.out.println("2. Multiply numbers");
+        System.out.println("3. Divide numbers");
+        System.out.println("4. Delete numbers");
+        System.out.print("Please select 1-4 : ");
         int choice = scanner.nextInt();
 
-        System.out.print("กรอกตัวเลขที่ 1 : ");
+        System.out.print("Fill in the numbers at 1 : ");
         int num1 = scanner.nextInt();
-        System.out.print("กรอกตัวเลขที่ 2 : ");
+        System.out.print("Fill in the numbers at 2 : ");
         int num2 = scanner.nextInt();
         switch (choice) {
             case 1 :
-                System.out.println("ผลบอก: " +(num1 + num2));
+                System.out.println("Add numbers: " +(num1 + num2));
                 break;
             case 2 :
-                System.out.println("ผลคูณ: " +(num1 * num2));
+                System.out.println("Multiply numbers: " +(num1 * num2));
                 break;
             case 3 :
                 if (num2 !=0){
-                    System.out.println("ผลคูณ: " +(num1 / num2));
+                    System.out.println("Divide numbers: " +(num1 / num2));
                 } else{
-                    System.out.println("ไม่สามารถหารด้วยศูนย์ได้");
+                    System.out.println("cannot be divided by zero");
                 }
                 break;
             case 4 :
-                System.out.println("ผลลบ: " +(num1 - num2));
+                System.out.println("Delete numbers: " +(num1 - num2));
                 break;
             default:
-                System.out.println("การเลือกไม่ถูกต้อง กรุณาเลือก 1-4");
+                System.out.println("Incorrect selection. Please select. 1-4");
                 break;
         }
     }
