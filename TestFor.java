3f17563c8b32600a73df4ca228b93b746e202242
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-07-31 15:42:31 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,8 @@
+public class TestFor {
+    public static void main(String[] args) {
+        int multiplier = 2;
+        for (int i = 1; i <= 12; i++){
+            System.out.println(multiplier+" x " + i + " = "+(multiplier * i));
+        }
+    }
+}
