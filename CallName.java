7747271755f04b2776e348709e9f4152fa2c41c4
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-08-02 10:10:20 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,11 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            LoopName();
+        }
+    }
+    public static void LoopName() {
+        System.out.println("Phongsagorn Monghkol");
+    }
+}
+
