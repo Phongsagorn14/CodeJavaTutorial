8a4293c49fc9291ad6f71f7772eccebab24dbf67
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-08-02 14:43:04 +0700

    Create Ex5_2.java

---
+++ Ex5_2.java
@@ -0,0 +1,9 @@
+public class Ex5_2 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            int result = (int) Math.pow(x, 3); 
+            System.out.println("x = " + x + ", x^3 = " + result); 
+        }
+    }
+}
+
