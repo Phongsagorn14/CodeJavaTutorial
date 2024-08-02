ca140dff8fe2d423979365b704497878c2e74f10
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-08-02 13:59:47 +0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,14 @@
+public class LocalVars {
+    public static void main(String[] args) {
+        phuket();
+        rayoug();
+    }
+    public static void phuket() {
+        int birds = 500;
+        System.out.println("In phuket there are " +birds+ " birds.");
+     }
+     public static void rayoug() {
+        int birds = 3500;
+        System.out.println("In rayoug there are " +birds+ " birds.");
+     }
+}
