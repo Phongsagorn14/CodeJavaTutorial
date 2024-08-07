7f3e3e7b66f91e78cf865abcd92d9ef12157d764
Phongsagorn14 <phongsagorn254848@gmail.com>
2024-08-07 14:40:10 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    private String id;
+    private String name;
+    private double gpa;
+    
+    public void setDetails(String ID, String n, double GPA) {
+        id = ID;
+        name = n;
+        gpa = GPA;
+    }
+    public void showDetails() {
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("GPA : " + gpa);
+    }
+    public static void main(String[] args) {
+        Student sc1 = new Student();
+        sc1.setDetails("12123", "Phongsagorn", 3.50);  
+        sc1.showDetails();
+
+        Student sc2 = new Student();
+        sc2.setDetails("34123", "Anucha", 3.80);  
+        sc2.showDetails();
+    }
+}
+
